587d7adeea9c0f93549639906ca61cac414266ce
Artem Galay <iostema@mail.ru>
2022-10-25 09:28:08 +0300

    Initial Commit

---
+++ Test Task + JSON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test Task + JSON
+//
+//  Created by Артем Галай on 25.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
