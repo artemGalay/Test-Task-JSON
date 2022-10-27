46e81bc620b7b3a4ae000d721634435e12d9075f
Artem Galay <iostema@mail.ru>
2022-10-27 09:05:28 +0300

    Create DetailAlbumViewController.

---
+++ Test Task + JSON/Sources/Controllers/DetailAlbumViewController.swift
@@ -0,0 +1,117 @@
+//
+//  DetailAlbumViewController.swift
+//  Test Task + JSON
+//
+//  Created by Артем Галай on 27.10.22.
+//
+
+import UIKit
+
+class DetailAlbumViewController: UIViewController {
+
+    private lazy var albumLogo: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    private lazy var albumNameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "Name album"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private lazy var artistNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Name artist"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private lazy var releaseDataLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Release Data"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private lazy var trackCountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "10 tracks"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private lazy var collectionView: UICollectionView = {
+        let layout  = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 5
+        let collectionView = UICollectionView()
+        collectionView.backgroundColor = .white
+        collectionView.bounces = false
+        collectionView.register(SongsCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+
+    private lazy var stackView = UIStackView(arrangeSubview: [albumNameLabel,
+                                                              artistNameLabel,
+                                                              releaseDataLabel,
+                                                              trackCountLabel],
+                                             axis: .vertical,
+                                             spacing: 10,
+                                             distribution: .fillProportionally)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupHierarchy()
+        setupLayout()
+    }
+
+    private func setupHierarchy() {
+        view.addSubview(albumLogo)
+        view.addSubview(stackView)
+        view.addSubview(collectionView)
+
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            albumLogo.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            albumLogo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            albumLogo.heightAnchor.constraint(equalToConstant: 100),
+            albumLogo.widthAnchor.constraint(equalToConstant: 100),
+
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            stackView.leadingAnchor.constraint(equalTo: albumLogo.trailingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+
+            collectionView.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 0),
+            collectionView.leadingAnchor.constraint(equalTo: albumLogo.trailingAnchor, constant: 10),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10)
+        ])
+    }
+}
+
+extension AlbumsViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! SongsCollectionViewCell
+        cell.nameSongLabel = "Name song"
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: collectionView.frame.width, height: 20)
+    }
+}
