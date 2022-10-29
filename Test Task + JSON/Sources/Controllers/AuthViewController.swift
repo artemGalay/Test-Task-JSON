d918098f687a0455cf4fef3401063cd9e0228931
Artem Galay <iostema@mail.ru>
2022-10-29 18:20:45 +0300

    Add func setModel().

--- Test Task + JSON/Sources/Controllers/AuthViewController.swift
+++ Test Task + JSON/Sources/Controllers/AuthViewController.swift
@@ -52,14 +52,14 @@ class AuthViewController: UIViewController {
     }()
 
     private lazy var textFieldStackView = UIStackView(arrangeSubview: [emailTextField, passwordTextField],
-                                                  axis: .vertical,
-                                                  spacing: 10,
-                                                  distribution: .fillProportionally)
+                                                      axis: .vertical,
+                                                      spacing: 10,
+                                                      distribution: .fillProportionally)
 
     private lazy var buttonsStackView = UIStackView(arrangeSubview: [signInButton, signUpButton],
-                                                  axis: .horizontal,
-                                                  spacing: 10,
-                                                  distribution: .fillEqually)
+                                                    axis: .horizontal,
+                                                    spacing: 10,
+                                                    distribution: .fillEqually)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -93,14 +93,43 @@ class AuthViewController: UIViewController {
     }
 
     @objc func signInTapped() {
-       let navigationViewController = UINavigationController(rootViewController: AlbumsViewController())
-        navigationViewController.modalPresentationStyle = .fullScreen
-        present(navigationViewController, animated: true)
+
+        let mail = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        let user = findUserDataBase(mail: mail)
+
+        if user == nil {
+            loginLabel.text = "User not found"
+            loginLabel.textColor = .red
+        } else if user?.password == password {
+            let navigationViewController = UINavigationController(rootViewController: AlbumsViewController())
+            navigationViewController.modalPresentationStyle = .fullScreen
+            present(navigationViewController, animated: true)
+
+            guard let activeUser = user else { return }
+            DataBase.shared.saveActiveUser(user: activeUser )
+        } else {
+            loginLabel.text = "Wrong password"
+            loginLabel.textColor = .red
+
+        }
     }
 
     @objc func signUpTapped() {
         let viewController = SignUpViewController()
         present(viewController, animated: true)
+    }
+
+    private func findUserDataBase(mail: String) -> User? {
+        let dataBase = DataBase.shared.users
+        print(dataBase)
+
+        for user in dataBase {
+            if user.email == mail {
+                return user
+            }
+        }
+        return nil
     }
 }
 
--- Test Task + JSON/Sources/Services/UserDefaultsManager.swift
+++ Test Task + JSON/Sources/Services/UserDefaultsManager.swift
@@ -13,10 +13,12 @@ class DataBase {
 
     enum SettingKeys: String {
         case users
+        case activeUser
     }
 
     let defaults = UserDefaults.standard
     let userKey = SettingKeys.users.rawValue
+    let activeUserKey = SettingKeys.activeUser.rawValue
 
     var users: [User] {
         get {
@@ -36,5 +38,23 @@ class DataBase {
 
         let user = User(firstName: firstName, secondName: secondName, phone: phone, email: email, password: password, age: age)
         users.insert(user, at: 0)
+    }
+
+    var activeUser: User?{
+        get {
+            if let data = defaults.value(forKey: activeUserKey) as? Data {
+                return try! PropertyListDecoder().decode(User.self, from: data)
+            } else {
+                return nil
+            }
+        }
+        set {
+            if let data = try? PropertyListEncoder().encode(newValue) {
+                defaults.set(data, forKey: userKey)            }
+        }
+    }
+
+    func saveActiveUser(user: User) {
+        activeUser = user
     }
 }
